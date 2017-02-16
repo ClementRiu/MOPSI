f61bdb1cb0636454951134deadb2dfff30cfff55
ClementRiu <riuclement@gmail.com>
2017-02-16 12:36:21 +0100

    Adding the GCP code They are useless

--- main.cpp
+++ main.cpp
@@ -6,37 +6,108 @@
 #include "imageProcessingFunc.h"
 #include "shorterPath.h"
 
+void
+printResults(bool useEdge, bool useGCP, Img &imageL, Img &imageR, int largeur,
+             int hauteur, Imagine::IntPoint2 position) {
+    //! Print results for computation in the current window.
+    int verticalOffSet = 5;
+    int GCPWidth = 2 * useGCP;
+    assert(verticalOffSet >= GCPWidth);
+    std::cout << "Calcul d'une carte de disparité : " << std::endl;
+    std::cout << "Edges use : " << useEdge << "  /  ";
+    std::cout << "GCP use : " << useGCP << std::endl;
+
+    Imagine::Image<byte, 2> imageLEdges(largeur, hauteur);
+    Imagine::Image<byte, 2> imageREdges(largeur, hauteur);
+    if (useEdge) {
+        //! Affichage des bords des images
+        edgeDetector(imageL, imageLEdges);
+        display(grey(imageLEdges), Imagine::IntPoint2(largeur + 2, 1));
+        edgeDetector(imageR, imageREdges);
+        display(grey(imageREdges),
+                Imagine::IntPoint2(largeur + 2, hauteur + 2));
+    }
+
+    //! Computation of disparity map with edges
+    Imagine::Image<int, 2> disparity(largeur, hauteur);
+    disparity = initDisparity(largeur, hauteur);
+    int *dispMaxTab = new int[hauteur];
+    int *dispMinTab = new int[hauteur];
+    for (int ligne = 0; ligne < hauteur; ++ligne) {
+        dispMaxTab[ligne] = -1;
+        dispMinTab[ligne] = int(pow(2, 64));
+    }
+
+
+    // Time calculation to compare different algorithm
+    struct timespec start, finish;
+    double elapsed;
+    std::cout << "Début du calcul..." << std::endl;
+    clock_t time1 = clock_gettime(CLOCK_MONOTONIC, &start);
+
+#pragma omp parallel for
+    for (int ligne = verticalOffSet;
+         ligne < hauteur - verticalOffSet; ++ligne) {
+        disparityComputationG(largeur, imageL, imageR, ligne, disparity,
+                              dispMaxTab[ligne], dispMinTab[ligne],
+                              imageLEdges, imageREdges, useEdge, useGCP,
+                              GCPWidth);
+    }
+
+    clock_t time2 = clock_gettime(CLOCK_MONOTONIC, &finish);
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    std::cout << "Calcul fait en: " << (elapsed) << " s" << std::endl;
+
+    int dispMax = -1;
+    int dispMin = int(pow(2, 64));
+    for (int ligne = 0; ligne < hauteur; ++ligne) {
+        if (dispMaxTab[ligne] > dispMax) {
+            dispMax = dispMaxTab[ligne];
+        }
+        if (dispMinTab[ligne] < dispMin) {
+            dispMin = dispMinTab[ligne];
+        }
+    }
+
+    //! Display the depth map with edge.
+    Imagine::Image<Imagine::Color, 2> depth(largeur, hauteur);
+    depth = disparityToDepth(disparity, dispMax, dispMin, largeur, hauteur);
+    display(depth, position);
+}
+
 int main() {
     //!Chargement et affichage des images.
-    Img image_L;
-    Img image_R;
+    Img imageL;
+    Img imageR;
     // Chargement des images.
     // "cone" ou "tsuku"
-    if (loadingImage(image_L, image_R, "cone") == 1) {
+    if (loadingImage(imageL, imageR, "cone") == 1) {
         std::cerr << "Mauvais chargement de fichier" << std::endl;
         return 0;
     }
 
     // Affichage d'information sur les images.
-    int largeur = image_L.width(); // Identique pour les deux images
-    int hauteur = image_L.height();
+    int largeur = imageL.width(); // Identique pour les deux images
+    int hauteur = imageL.height();
     std::cout << "Dimension de l'image : " << largeur << " * " << hauteur
               << std::endl;
     int taille = largeur * hauteur;
     std::cout << taille << std::endl;
 
     // Affichage des images.
-    Imagine::openWindow(3 * largeur + 4, 2 * hauteur + 3, "Disparity map");
-    display(grey(image_L), Imagine::IntPoint2(1, 1));
-    display(grey(image_R), Imagine::IntPoint2(1, hauteur + 1));
+    Imagine::openWindow(4 * largeur + 5, 2 * hauteur + 3, "Disparity map");
+    display(grey(imageL), Imagine::IntPoint2(1, 1));
+    display(grey(imageR), Imagine::IntPoint2(1, hauteur + 1));
 
+/*
     //! Affichage des bords des images
-    Imagine::Image<byte, 2> image_L_edges(largeur, hauteur);
-    edgeDetector(image_L, image_L_edges);
-    display(grey(image_L_edges), Imagine::IntPoint2(largeur + 2, 1));
-    Imagine::Image<byte, 2> image_R_edges(largeur, hauteur);
-    edgeDetector(image_R, image_R_edges);
-    display(grey(image_R_edges), Imagine::IntPoint2(largeur + 2, hauteur + 2));
+    Imagine::Image<byte, 2> imageLEdges(largeur, hauteur);
+    edgeDetector(imageL, imageLEdges);
+    display(grey(imageLEdges), Imagine::IntPoint2(largeur + 2, 1));
+    Imagine::Image<byte, 2> imageREdges(largeur, hauteur);
+    edgeDetector(imageR, imageREdges);
+    display(grey(imageREdges), Imagine::IntPoint2(largeur + 2, hauteur + 2));
 
     //! Computation of disparity map with edges
     Imagine::Image<int, 2> disparityEdge(largeur, hauteur);
@@ -56,11 +127,11 @@ int main() {
     clock_t time1Edge = clock_gettime(CLOCK_MONOTONIC, &startEdge);
 
 #pragma omp parallel for
-    for (int ligne = 0; ligne < hauteur - 5; ++ligne) {
-        disparityComputationEdgy(largeur, image_L, image_R, ligne,
+    for (int ligne = 5; ligne < hauteur - 5; ++ligne) {
+        disparityComputationEdgy(largeur, imageL, imageR, ligne,
                                  disparityEdge, dispMaxTabEdge[ligne],
-                                 dispMinTabEdge[ligne], image_L_edges,
-                                 image_R_edges);
+                                 dispMinTabEdge[ligne], imageLEdges,
+                                 imageREdges);
     }
 
     clock_t time2Edge = clock_gettime(CLOCK_MONOTONIC, &finishEdge);
@@ -84,8 +155,8 @@ int main() {
     depthEdge = disparityToDepth(disparityEdge, dispMaxEdge, dispMinEdge,
                                  largeur, hauteur);
     display(depthEdge, Imagine::IntPoint2(largeur * 2 + 3, 1));
-
-
+*/
+/*
     //! Calcul de la carte de disparité
     Imagine::Image<int, 2> disparity(largeur, hauteur);
     disparity = initDisparity(largeur, hauteur);
@@ -104,8 +175,8 @@ int main() {
     clock_t time1 = clock_gettime(CLOCK_MONOTONIC, &start);
 
 #pragma omp parallel for
-    for (int ligne = 0; ligne < hauteur; ++ligne) {
-        disparityComputation(largeur, image_L, image_R, ligne, disparity,
+    for (int ligne = 5; ligne < hauteur - 5; ++ligne) {
+        disparityComputation(largeur, imageL, imageR, ligne, disparity,
                              dispMaxTab[ligne], dispMinTab[ligne]);
     }
 
@@ -129,7 +200,19 @@ int main() {
     Imagine::Image<Imagine::Color, 2> depth(largeur, hauteur);
     depth = disparityToDepth(disparity, dispMax, dispMin, largeur, hauteur);
     display(depth, Imagine::IntPoint2(largeur * 2 + 3, hauteur + 2));
+*/
 
+    printResults(false, true, imageL, imageR, largeur, hauteur,
+                 Imagine::IntPoint2(largeur * 2 + 3, 1));
+
+    printResults(false, false, imageL, imageR, largeur, hauteur,
+                 Imagine::IntPoint2(largeur * 2 + 3, hauteur + 2));
+
+    printResults(true, true, imageL, imageR, largeur, hauteur,
+                 Imagine::IntPoint2(largeur * 3 + 4, 1));
+
+    printResults(true, false, imageL, imageR, largeur, hauteur,
+                 Imagine::IntPoint2(largeur * 3 + 4, hauteur + 2));
 
     Imagine::endGraphics();
 
--- shorterPath.h
+++ shorterPath.h
@@ -7,6 +7,7 @@
 
 #include "library.h"
 #include "imageProcessingFunc.h"
+#include "groundControlPoint.h"
 
 //! Initialise the disparity map.
 Imagine::Image<int, 2> initDisparity(int largeur, int hauteur);
@@ -15,6 +16,7 @@ Imagine::Image<int, 2> initDisparity(int largeur, int hauteur);
 Imagine::Image<int, 2>
 initCost(int largeur, int ligne, const Img &image1, const Img &image2);
 
+/*
 //! shorterPath computation with dynamic programming
 Imagine::Image<int, 1>
 shorterPath(int largeur, const Imagine::Image<int, 2> &cost, int ligne);
@@ -35,5 +37,18 @@ void disparityComputationEdgy(int largeur, const Img &image1, const Img &image2,
                               int ligne, Imagine::Image<int, 2> &disparity,
                               int &dispMax, int &dispMin, const Img &image1Edge,
                               const Img &image2Edge);
+*/
+
+//! shorterPath computation with dynamic programming
+Imagine::Image<int, 1>
+shorterPathG(int largeur, const Imagine::Image<int, 2> &cost, int ligne,
+             const Img &image1Edge, const Img &image2Edge, bool useEdge);
+
+//! depth computation
+void disparityComputationG(int largeur, const Img &image1, const Img &image2,
+                           int ligne, Imagine::Image<int, 2> &disparity,
+                           int &dispMax, int &dispMin, const Img &image1Edge,
+                           const Img &image2Edge, bool useEdge, bool useGCP,
+                           int GCPWidth);
 
 #endif //PROJECT_SHORTERPATH_H
